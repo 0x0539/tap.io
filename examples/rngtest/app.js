6c06acb671fc03ed6606e014e0a933cbac459bc6
Sebastian Goodman <sebastian.goodman@gmail.com>
2013-04-15 23:43:14 -0700

    shortening rng example, does not need ejs

--- examples/rngtest/app.js
+++ examples/rngtest/app.js
@@ -1,17 +1,5 @@
 var tapio = require('tap.io');
 
 var app = new tapio.App();
-
-var ejsScope = {scripts: []};
-app.forAllResources(function(resource){
-  if (resource instanceof tapio.JsResource)
-    ejsScope.scripts.push(resource.name);
-});
-
-var testHtml = new tapio.FileResource('/test.html.raw', __dirname + '/test.html.ejs', 'text/html');
-var testHtmlEjs = new tapio.EjsResource('/test.html', testHtml, ejsScope);
-
-app.addResource(testHtml);
-app.addResource(testHtmlEjs);
-
+app.addResource(new tapio.FileResource('/test.html', __dirname + '/test.html', 'text/html'));
 app.start(8080);

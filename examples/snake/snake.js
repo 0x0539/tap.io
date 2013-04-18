3bfbe34080b336aff046918d674e6cbbb2e9fd28
Sebastian <sebastian.goodman@gmail.com>
2013-04-18 16:19:09 -0700

    snake game now playable

--- examples/snake/snake.js
+++ examples/snake/snake.js
@@ -5,11 +5,21 @@ var FOOD_COUNT = 5;
 var SNAKE_GROWTH_RATE = 10;
 var SNAKE_START_LENGTH = 50;
 var SNAKE_SPEED = 1;
+var SNAKE_WIDTH = 3;
+var FOOD_RADIUS = 3;
 
 var MIN_X = 0;
-var MAX_X = 200;
+var MAX_X = 500;
 var MIN_Y = 0;
-var MAX_Y = 200;
+var MAX_Y = 500;
+
+var SnakeParameters = {};
+SnakeParameters.SNAKE_WIDTH = SNAKE_WIDTH;
+SnakeParameters.FOOD_RADIUS = FOOD_RADIUS;
+SnakeParameters.MIN_X = MIN_X;
+SnakeParameters.MAX_X = MAX_X;
+SnakeParameters.MIN_Y = MIN_Y;
+SnakeParameters.MAX_Y = MAX_Y;
 
 var SnakeEngine = function() {
   tapio.Serializer.registerInstance(this, SnakeEngine);
@@ -23,6 +33,32 @@ SnakeEngine.prototype.update = function(state) {
   }
   for(var sessionId in this.players){
     this.players[sessionId].move();
+  }
+  for(var sessionId1 in this.players){
+    var player1 = this.players[sessionId1];
+    for(var sessionId2 in this.players){
+      var player2 = this.players[sessionId2];
+      if(player1.isCollidingWithPlayer(player2)){
+        player1.markForDeath();
+      }
+    }
+  }
+  for(var sessionId in this.players){
+    var player = this.players[sessionId];
+    if(player.isMarkedForDeath()){
+      player.die();
+    }
+  }
+  for(var f = 0; f < this.food.length; f++){
+    var food = this.food[f];
+    for(var sessionId in this.players){
+      var player = this.players[sessionId];
+      if(player.isCollidingWithFood(food)) {
+        player.feed(food);
+        this.food[f] = new Food(this.randomX(), this.randomY());
+        break;
+      }
+    }
   }
 };
 SnakeEngine.prototype.validate = function(state, event) { };
@@ -90,6 +126,31 @@ var Segment = function(x1, y1, x2, y2){
   this.x2 = x2;
   this.y2 = y2;
 };
+Segment.prototype.minX = function(){
+  return Math.min(this.x1, this.x2);
+};
+Segment.prototype.maxX = function(){
+  return Math.max(this.x1, this.x2);
+};
+Segment.prototype.minY = function(){
+  return Math.min(this.y1, this.y2);
+};
+Segment.prototype.maxY = function(){
+  return Math.max(this.y1, this.y2);
+};
+Segment.prototype.overlaps = function(other, margin){
+  if(margin == null) 
+    margin = 0;
+  if(this.maxX() + margin < other.minX())
+    return false;
+  if(this.minX() - margin > other.maxX())
+    return false;
+  if(this.maxY() + margin < other.minY())
+    return false;
+  if(this.minY() - margin > other.maxY())
+    return false;
+  return true;
+};
 
 var Snake = function(x, y) {
   tapio.Serializer.registerInstance(this, Snake);
@@ -118,7 +179,7 @@ Snake.prototype.move = function() {
 Snake.prototype.getLength = function() {
   return SNAKE_START_LENGTH + this.eaten * SNAKE_GROWTH_RATE;
 };
-Snake.prototype.feed = function() {
+Snake.prototype.feed = function(food) {
   this.eaten++;
 };
 Snake.prototype.getSegments = function() {
@@ -141,10 +202,32 @@ Snake.prototype.getSegments = function() {
   segments.push(new Segment(j1.x, j1.y, j1.x-j1.dx*remainder, j1.y-j1.dy*remainder));
   return segments;
 };
+Snake.prototype.isCollidingWithSnake = function(other){
+  var head = new Segment(this.x, this.y, this.x, this.y);
+
+  var segments = other.getSegments();
+
+  if (this == other) {
+    segments = segments.slice(2);
+  }
+
+  for (var i = 0; i < segments.length; i++){
+    var segment = segments[i];
+    if (segment.overlaps(head, SNAKE_WIDTH * 2)) {
+      return true;
+    }
+  }
+  return false;
+};
+Snake.prototype.isCollidingWithFood = function(food){
+  var distance = Math.sqrt(Math.pow(food.x - this.x, 2) + Math.pow(food.y - this.y, 2));
+  return distance <= FOOD_RADIUS + SNAKE_WIDTH;
+};
 
 var Player = function() {
   tapio.Serializer.registerInstance(this, Player);
   this.snake = null;
+  this.markedForDeath = false;
 };
 Player.prototype.turn = function(dx, dy){
   if (this.snake) 
@@ -154,9 +237,36 @@ Player.prototype.respawn = function(snake){
   if (this.snake == null)
     this.snake = snake;
 };
+Player.prototype.markForDeath = function(){
+  this.markedForDeath = true;
+};
+Player.prototype.isMarkedForDeath = function(){
+  return this.markedForDeath;
+};
+Player.prototype.die = function(){
+  this.snake = null;
+  this.markedForDeath = false;
+};
+Player.prototype.isDead = function(){
+  return this.snake == null;
+};
 Player.prototype.move = function(){
   if (this.snake)
     this.snake.move();
+};
+Player.prototype.isCollidingWithPlayer = function(other){
+  if (this.snake && other.snake)
+    return this.snake.isCollidingWithSnake(other.snake);
+  return false;
+};
+Player.prototype.isCollidingWithFood = function(food){
+  if (this.snake)
+    return this.snake.isCollidingWithFood(food);
+  return false;
+};
+Player.prototype.feed = function(food){
+  if (this.snake)
+    this.snake.feed(food);
 };
 
 tapio.Serializer.registerType(SnakeEngine);
@@ -167,3 +277,4 @@ tapio.Serializer.registerType(Snake);
 tapio.Serializer.registerType(Player);
 
 exports.SnakeEngine = SnakeEngine;
+exports.SnakeParameters = SnakeParameters;
